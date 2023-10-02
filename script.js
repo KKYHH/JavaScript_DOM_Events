ab3d467bd05bc314d78a43abb4499506eef49b8f
KYHH <kyhh93@naver.com>
2023-10-02 21:46:44 +0900

    JavaScript DOM and Events

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+}
+
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  displayMessage('Start guessing ...');
+  document.querySelector('.guess').value = '';
+});
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    displayMessage('❌ No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('👍 You did it!')
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high !' : 'Too low !')
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 you lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+})
+
