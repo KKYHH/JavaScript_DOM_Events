71e36c528306a397850ec6aa575d901de0d62243
KYHH <kyhh93@naver.com>
2023-10-04 23:17:21 +0900

    refact : if

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      displayMessage(guess > secretNumber ? 'Too high !' : 'Too low !')
+      displayMessage(guess > secretNumber ? 'Too high ⬆️ !' : 'Too low ⬇️ !')
       score--;
       document.querySelector('.score').textContent = score;
     } else {
